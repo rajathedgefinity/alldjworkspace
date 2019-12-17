27f49a05b1fd23d145ebb82d6fb3ad48bef56466
rajathedgefinity <rajathedgefinity@gmail.com>
2019-12-17 13:32:42 +0530

    Written some testcases to board_topics and raised 404 with try and except statements

--- tryingforum/boards/urls.py
+++ tryingforum/boards/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('boards/<pk>/', views.board_topics, name='board_topics'),
 ]
--- tryingforum/boards/views.py
+++ tryingforum/boards/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Board
 
 # Create your views here.
 def home(request):
     brs = Board.objects.all()
     return render(request,'boards/home.html',{'brs':brs})
+
+def board_topics(request, pk):
+    try:
+        br = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
+    return render(request,'boards/topics.html', {'br':br})
