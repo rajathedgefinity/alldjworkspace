a86bd444d501e9db376d8ad0d3401f0e3b850532
rajathedgefinity <rajathedgefinity@gmail.com>
2019-12-17 12:36:28 +0530

    Added Templates and Static Files made Forum Looks Great and Good.

--- tryingforum/boards/views.py
+++ tryingforum/boards/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World!")
+    brs = Board.objects.all()
+    return render(request,'boards/home.html',{'brs':brs})
